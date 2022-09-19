b01379eb91a4d344afd463d95206176fef080acd
Kim <mykim250795@gmail.com>
2022-09-19 18:15:52 -0400

    adding custom for aboutus

--- js/aboutus.js
+++ js/aboutus.js
@@ -1,15 +1,22 @@
+
+const body = document.querySelector("body");
 const darkmode = document.querySelector("#darkmode");
 const lightmode = document.querySelector("#lightmode");
 
 function setDarkMode(){
-    body.style.backgroundColor="white";
-    body.style.color="black";
-}
-
-function setLightMode(){
     body.style.backgroundColor="black";
     body.style.color="white";
 }
 
+function setLightMode(){
+    body.style.backgroundColor="white";
+    body.style.color="black";
+}
+
 darkmode.addEventListener('click',setDarkMode);
 lightmode.addEventListener('click',setLightMode);
+
+
+
+
+
